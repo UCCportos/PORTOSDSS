9ce9294f6747ae1de177adc5b8d9fe3d6d59bf57
UCCportos <105803014+UCCportos@users.noreply.github.com>
2022-06-09 16:25:23 +0100

    Update settings.py

--- DSS/settings.py
+++ DSS/settings.py
@@ -83,16 +83,16 @@ WSGI_APPLICATION = 'DSS.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'solar',
-        'USER': 'postgres',
-        'PASSWORD': 'Oe14d009$',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'solar',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Oe14d009$',
+#         'HOST': 'localhost',
+#        'PORT': '5432'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
