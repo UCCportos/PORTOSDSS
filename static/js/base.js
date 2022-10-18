0b3bb3a081a1051503ff7e306945e8b3a533c9c3
swaminathan <swaminathan@swaminathansAir.bbrouter>
2022-10-18 21:31:57 +0530

    save storage

--- static/js/base.js
+++ static/js/base.js
@@ -18,11 +18,9 @@ $.when( $.ready ).then(function() {
    });
 
   function saveStorage(event){
-    console.log('Save storage',event.name)
     const formElement = sessionStorage.getItem("dss_form");
     const location_value = event.value;
     const inputName = event.name;
     const form = {...JSON.parse(formElement),[inputName]:location_value}; // looping through form challange is here due to key
     sessionStorage.setItem("dss_form",JSON.stringify(form))
-    console.log('Save storage',sessionStorage.getItem("dss_form"))
   } 
